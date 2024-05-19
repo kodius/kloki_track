ac43d7ef139d897d8e9041083169bdf8860c8c77
drKreso <kresimir.bojcic@gmail.com>
2024-05-19 22:07:00 +0200

    Init

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+ <script>
+  import "../app.css";
+</script>
+
+<div class="flex min-h-screen">
+  <div class="w-64 bg-gray-200 p-5 shadow-lg">
+    <nav>
+      <ul>
+        <li class="mb-4"><a href="/" class="text-blue-500 hover:text-blue-700">Home</a></li>
+        <li class="mb-4"><a href="/about" class="text-blue-500 hover:text-blue-700">About</a></li>
+        <li class="mb-4"><a href="/contact" class="text-blue-500 hover:text-blue-700">Contact</a></li>
+      </ul>
+    </nav>
+  </div>
+  <div class="flex-1 p-5 flex items-center justify-center">
+   <slot/>
+  </div>
+</div>
+
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button/index.js";
+  import * as Card from "$lib/components/ui/card/index.js";
+  import * as Select from "$lib/components/ui/select/index.js";
+  import { Input } from "$lib/components/ui/input/index.js";
+  import { Label } from "$lib/components/ui/label/index.js";
+
+  const clients = [
+    {
+      value: "optilogic",
+      label: "Optilogic"
+    },
+    {
+      value: "hbi",
+      label: "HBI"
+    },
+    {
+      value: "poplin",
+      label: "Poplin"
+    }
+  ];
+</script>
+
+<Card.Root class="w-[350px]">
+  <Card.Header>
+    <Card.Title>Create project</Card.Title>
+    <Card.Description>Create your new project</Card.Description>
+  </Card.Header>
+  <Card.Content>
+    <form>
+      <div class="grid w-full items-center gap-4">
+        <div class="flex flex-col space-y-1.5">
+          <Label for="name">Name</Label>
+          <Input id="name" placeholder="Name of your project" autoComplete="off" />
+        </div>
+        <div class="flex flex-col space-y-1.5">
+          <Label for="framework">Client</Label>
+          <Select.Root>
+            <Select.Trigger id="client">
+              <Select.Value placeholder="Select" />
+            </Select.Trigger>
+            <Select.Content>
+              {#each clients as client}
+                <Select.Item value={client.value} label={client.label}
+                  >{client.label}</Select.Item
+                >
+              {/each}
+            </Select.Content>
+          </Select.Root>
+        </div>
+      </div>
+    </form>
+  </Card.Content>
+  <Card.Footer class="flex justify-between">
+    <Button variant="outline">Cancel</Button>
+    <Button>Save</Button>
+  </Card.Footer>
+</Card.Root>
