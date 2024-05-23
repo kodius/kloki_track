998a9ecb35fb3fe6273eafe18c4e93fdebf77fa6
tomislavculig <tomislav@kodius.com>
2024-05-23 11:10:47 +0200

    Move to separate database

--- backend/config/dev.exs
+++ backend/config/dev.exs
@@ -5,11 +5,10 @@ config :backend, Backend.Repo,
   username: "postgres",
   password: "",
   hostname: "localhost",
-  database: "boro_development",
+  database: "tracker_development",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10,
-  migration_source: "tracker_schema_migrations"
+  pool_size: 10
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
--- backend/priv/repo/migrations/20240521141716_create_projects_and_clients.exs
+++ backend/priv/repo/migrations/20240521141716_create_projects_and_clients.exs
@@ -2,15 +2,15 @@ defmodule Backend.Repo.Migrations.CreateProjectsAndClients do
   use Ecto.Migration
 
   def change do
-    create table(:clients, prefix: "tracker") do
+    create table(:clients) do
       add :name, :string, null: false
       add :archived, :boolean, default: false, null: false
 
       timestamps()
     end
 
-    create table(:projects, prefix: "tracker") do
-      add :client_id, references(:clients, prefix: "tracker", on_delete: :nothing), null: false
+    create table(:projects) do
+      add :client_id, references(:clients, on_delete: :nothing), null: false
       add :name, :string, null: false
       add :public, :boolean, default: false, null: false
       add :archived, :boolean, default: false, null: false
@@ -18,6 +18,6 @@ defmodule Backend.Repo.Migrations.CreateProjectsAndClients do
       timestamps()
     end
 
-    create index(:projects, [:client_id], prefix: "tracker")
+    create index(:projects, [:client_id])
   end
 end
