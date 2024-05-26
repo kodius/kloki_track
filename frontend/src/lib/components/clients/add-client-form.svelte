aa50610ae0eb0ff4f33f1a92d23f2ddea2e4bccc
drKreso <kresimir.bojcic@gmail.com>
2024-05-26 14:46:40 +0200

    Add client on Enter

--- frontend/src/lib/components/clients/add-client-form.svelte
+++ frontend/src/lib/components/clients/add-client-form.svelte
@@ -1,31 +1,38 @@
 <script>
-	const { addNewClient } = $props();
-	let newClientName = $state('');
+    const { addNewClient } = $props();
+    let newClientName = $state("");
 
-	async function handleSubmit() {
-		await addNewClient(newClientName);
-		newClientName = '';
-	}
+    async function handleSubmit() {
+        await addNewClient(newClientName);
+        newClientName = '';
+    }
+
+    function handleKeydown(event) {
+        if (event.keyCode === 13 && newClientName.trim() !== '') {
+            handleSubmit();
+        }
+    }
 </script>
 
 <div class="w-full justify-start flex">
-	<div class="w-full xl:w-1/3 flex pt-6 pb-6 justify-start">
-		<div class="flex-grow">
-			<input
-				type="text"
-				placeholder="Add new client"
-				bind:value={newClientName}
-				class="w-full p-2 border rounded"
-			/>
-		</div>
-		<div class="ml-4">
-			<button
-				on:click={handleSubmit}
-				class="ml-4 p-2 bg-blue-500 text-white rounded disabled:opacity-50 disabled:cursor-not-allowed"
-				disabled={newClientName.trim() === ''}
-			>
-				Add
-			</button>
-		</div>
-	</div>
+    <div class="w-full xl:w-1/3 flex pt-6 pb-6 justify-start">
+        <div class="flex-grow">
+            <input
+                type="text"
+                placeholder="Add new client"
+                bind:value={newClientName}
+                on:keydown={handleKeydown}
+                class="w-full p-2 border rounded"
+            />
+        </div>
+        <div class="ml-4">
+            <button
+                on:click={handleSubmit}
+                class="ml-4 p-2 bg-blue-500 text-white rounded disabled:opacity-50 disabled:cursor-not-allowed"
+                disabled={newClientName.trim() === ''}
+            >
+                Add
+            </button>
+        </div>
+    </div>
 </div>
